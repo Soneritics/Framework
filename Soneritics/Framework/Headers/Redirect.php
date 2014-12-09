bae936e14d1d947b00407a855477e5fccca2ab59
Jordi Jolink <info@soneritics.nl>
2014-12-09 21:23:53 +0100

    Namespace change.

--- Soneritics/Framework/Headers/Redirect.php
+++ Soneritics/Framework/Headers/Redirect.php
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-namespace Soneritics\Framework\Headers;
+namespace Framework\Headers;
 
 /**
  * Simple class that redirects to a given URL.
