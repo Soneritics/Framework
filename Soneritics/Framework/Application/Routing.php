1ed55da1f2e91e0caee76cdfb6195a4e35e01c9f
Jordi Jolink <info@soneritics.nl>
2014-12-18 22:25:50 +0100

    Fully functional Routing class.

--- Soneritics/Framework/Application/Routing.php
+++ Soneritics/Framework/Application/Routing.php
@@ -24,8 +24,11 @@
  */
 namespace Framework\Application;
 
+use Framework\IO\URI;
+
 /**
- * 
+ * Routing class.
+ * Handles mapping URLs to the correct module, controller and function.
  * 
  * @author Jordi Jolink
  * @date 18-9-2014
@@ -34,39 +37,168 @@ class Routing
 {
     private $routing, $route = array();
 
+	/**
+	 * Constructor saved the routing array in a property.
+	 * 
+	 * @param array $routing
+	 */
     public function __construct(array $routing)
     {
         $this->routing = $routing;
+		$this->init();
     }
 
+	/**
+	 * Find the route.
+	 * 
+	 * @return bool Route has been found.
+	 */
+	private function init()
+	{
+		// Get the current URI
+		$uri = new URI;
+		$url = $uri->getURL();
+
+		// Check for full url in the routes
+		if (isset($this->routing[$url])) {
+			return $this->parseRoute(
+				$url,
+				$uri,
+				$this->routing[$url]
+			);
+		}
+
+		// Get wildcarded urls
+		$wildcardedURLs = preg_grep('/\*/', array_keys($this->routing));
+		if (empty($wildcardedURLs)) {
+			return false;
+		}
+
+		// Check for partial, wildcarded url
+		$parts = explode('/', $url);
+		while (count($parts) > 1) {
+			$check = implode('/', $parts) . '/*';
+			if (in_array($check, $wildcardedURLs)) {
+				return $this->parseRoute(
+					$check,
+					$uri,
+					$this->routing[$check]
+				);
+			}
+
+			array_pop($parts);
+		}
+
+		// No route found
+		return false;
+	}
+
+	/**
+	 * Store a route in the class' properties and fetch the parameters.
+	 * 
+	 * @param type $url
+	 * @param URI $uri
+	 * @param array $route
+	 * @return boolean
+	 */
+	private function parseRoute($url, URI $uri, array $route)
+	{
+		// Fetch the parameters from the URI
+		$route['Parameters'] = array();
+
+		if (substr($url, -1) === '*') {
+			$url = substr($url, 0, -1);
+		}
+
+		$stripped = substr($uri->getURL(), strlen($url));
+		$strippedParts = explode('/', $stripped);
+
+		if (!empty($strippedParts)) {
+			foreach ($strippedParts as $part) {
+				$var = trim($part);
+				if (strlen($var) > 0) {
+					$route['Parameters'][] = $var;
+				}
+			}
+		}
+
+		// Store the route
+		$this->route = $route;
+		return true;
+	}
+
+	/**
+	 * Check if a route can be found for the given URL.
+	 * 
+	 * @return bool Returns wether a route could be found.
+	 */
     public function canRoute()
     {
-        return true;
         return !empty($this->route);
     }
 
+	/**
+	 * Get the property from the route or null when it doesn't exist.
+	 * 
+	 * @param string $key
+	 * @return mixed
+	 */
+	private function get($key)
+	{
+		return isset($this->route[$key]) ? $this->route[$key] : null;
+	}
+
+	/**
+	 * Get the module name from the route.
+	 * 
+	 * @return string
+	 */
     public function getModule()
     {
-        return 'Pub';
+        return $this->get('Module');
     }
 
+	/**
+	 * Get the controller name from the route.
+	 * 
+	 * @return string
+	 */
     public function getController()
     {
-        return 'Login';
+        return $this->get('Controller');
     }
 
+	/**
+	 * Get the function name from the route.
+	 * 
+	 * @return string
+	 */
     public function getFunction()
     {
-        return 'index';
+        return $this->get('Function');
     }
 
-    public function getRequestType()
-    {
-        return 'Action';
-    }
-
+	/**
+	 * Get the parameters from the route.
+	 * 
+	 * @return array
+	 */
     public function getParams()
     {
-        return array();
+        return $this->get('Parameters');
+    }
+
+	/**
+	 * Get the request type.
+	 * 
+	 * @return string
+	 */
+    public function getRequestType()
+    {
+		if (isset($_SERVER['REQUEST_METHOD'])) {
+			return ucfirst(strtolower($_SERVER['REQUEST_METHOD']));
+		} else {
+			return 'Action';
+		}
     }
 }
