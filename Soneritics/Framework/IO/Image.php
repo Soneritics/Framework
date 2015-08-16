1a4e2b39694982bedee5915c8f0ccb9a361d8971
Jordi Jolink <info@soneritics.nl>
2015-08-16 13:51:47 +0200

    Extra image functions.

--- Soneritics/Framework/IO/Image.php
+++ Soneritics/Framework/IO/Image.php
@@ -35,8 +35,13 @@ class Image extends File
     // Constants for reading the $imageInfo properties.
     const WIDTH = 0;
     const HEIGHT = 1;
+    const IMAGETYPE = 2;
     const BITS = 'bits';
     const MIME = 'mime';
+
+    const IMAGE_GIF = 1;
+    const IMAGE_JPG = 2;
+    const IMAGE_PNG = 3;
 
     /**
      * Array as given from PHP's getimageinfo function.
@@ -102,5 +107,133 @@ class Image extends File
     {
         $this->readImageInfo();
         return $this->imageInfo[self::MIME];
+    }
+
+    /**
+     * Get a resource handle for the currently selected image file.
+     * @return resource
+     */
+    public function getHandle()
+    {
+        $this->readImageInfo();
+        $filename = $this->getPath() . $this->getFilename();
+        $result = null;
+
+        switch ($this->imageInfo[self::IMAGETYPE]) {
+            case self::IMAGE_GIF:
+                $result = @imagecreatefromgif($filename);
+                break;
+
+            case self::IMAGE_JPG:
+                $result = @imagecreatefromjpeg($filename);
+                break;
+
+            case self::IMAGE_PNG:
+                $result = @imagecreatefrompng($filename);
+                break;
+        }
+
+        if (is_resource($result)) {
+            return $result;
+        }
+
+		return null;
+    }
+
+    /**
+     * Rotate the image by a given angle.
+     * @param int $angle One of the ROTATION_* constants of this class.
+     * @return $this
+     */
+    public function rotate($angle)
+    {
+        // Clockwise instead of counter clockwise
+        $rotationAngle = 360 - $angle;
+
+        // Get the handle and throw an exception when it couldn't be loaded
+        $image = $this->getHandle();
+        if (empty($image)) {
+            throw new \Exception('Could not get image handle.');
+        }
+
+        // Rotate
+        $rotated = imagerotate($image, $rotationAngle, 1);
+
+        // Save the result over the current image
+        switch ($this->imageInfo[self::IMAGETYPE]) {
+            case self::IMAGE_GIF:
+                imagegif($rotated, $this->getPath() . $this->getFilename());
+                break;
+
+            case self::IMAGE_JPG:
+                imagejpeg($rotated, $this->getPath() . $this->getFilename(), 100);
+                break;
+
+            case self::IMAGE_PNG:
+                imagepng($rotated, $this->getPath() . $this->getFilename(), 0);
+                break;
+
+            default:
+                throw new \Exception('Unknown image format. Can not save.');
+        }
+
+        // Reset the imageInfo property as a new image has been created
+        $this->imageInfo = null;
+
+        // Chainable
+        return $this;
+    }
+
+    /**
+     * Convert to another image format.
+     * @param type $newImageType
+     * @throws \Exception
+     * @return $this
+     */
+    public function convert($newImageType)
+    {
+        // Get the handle and throw an exception when it couldn't be loaded
+        $handle = $this->getHandle();
+        if (empty($handle)) {
+            throw new \Exception('Could not get image handle.');
+        }
+
+        // Get filename without extension
+        $fileWithoutExt = $this->getPath() . substr(
+            $this->getFilename(),
+            0,
+            strrpos($this->getFilename(), '.')
+        );
+
+        // Convert
+        switch ($newImageType) {
+            case self::IMAGE_GIF:
+                $file = $fileWithoutExt . '.gif';
+                imagegif($handle, $file);
+                break;
+
+            case self::IMAGE_JPG:
+                $file = $fileWithoutExt . '.jpg';
+                imagejpeg($handle, $file, 100);
+                break;
+
+            case self::IMAGE_PNG:
+                $file = $fileWithoutExt . '.png';
+                imagepng($handle, $file, 0);
+                break;
+
+            default:
+                throw new \Exception('Unknown image format. Can not save.');
+        }
+
+        // Delete the old image
+        $this->delete();
+
+        // Reload the new image
+        $this->load($file);
+        $this->imageInfo = null;
+
+        // Chainable
+        return $this;
     }
 }
