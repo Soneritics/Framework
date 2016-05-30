b525d12fb6b3ee8a694002640410de1bcc82ac61
Jordi Jolink <info@soneritics.nl>
2016-05-30 20:44:08 +0200

    Fix sample file

--- Sample/CLI/sample.php
+++ Sample/CLI/sample.php
@@ -7,16 +7,6 @@ new Framework\Bootstrap('..');
 echo "It works\n";
 \Application::log('test');
 
-\Database\DatabaseConnectionFactory::create(
-    'sandbox',
-    [
-        'type' => 'PDOMySQL',
-        'dsn' => 'mysql:dbname=sandbox;host=localhost',
-        'user' => 'sandbox',
-        'password' => 'sandbox'
-    ]
-);
-
 $tables = \Database\Database::getTables();
 foreach ($tables as $table) {
     print_r($table);
